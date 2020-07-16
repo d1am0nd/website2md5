168b3d080aed83d46a5c1ec5fee54f052abd88e9
Dev Kordes <devkordes+github@gmail.com>
2020-07-16 02:11:49 +0200

    Concurrency

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"time"
+	"sync"
 	"bufio"
 	"crypto/md5"
 	"encoding/hex"
@@ -18,7 +19,7 @@ func main() {
 	inputFile := flag.String("input", "", "Input file")
 	outputFile := flag.String("output", "", "Output file")
 	printResponses := flag.Bool("print", false, "Whether to print responses. Default: no")
-	concurrent := flag.Int("concurrent", 1, "Concurrent requests. Default: 1")
+	// concurrent := flag.Int("concurrent", 1, "Concurrent requests. Default: 1")
 
 	flag.Parse()
 
@@ -30,7 +31,7 @@ func main() {
 		log.Fatal("Please provide output file using -output flag")
 	}
 
-	reader := bufio.NewReader(os.Stdin)
+	// reader := bufio.NewReader(os.Stdin)
 	currDir, err := os.Getwd()
 	
 	inputFp := filepath.Join(currDir, *inputFile)
@@ -44,44 +45,68 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	handleErr(err)
 
-	outputMap := map[int]string{}
+	// outputMap := map[int]string{}
 
 	lineCount := 0
 	start := time.Now()
 
+	var wg sync.WaitGroup 
+
+	lines := map[int](chan string){}
+
 	for scanner.Scan() {
-		res, err := http.Get(scanner.Text())
-		handleErr(err)
-		defer res.Body.Close()
+		url := scanner.Text()
 
-		if res.StatusCode >= 300 {
-			log.Fatal("Request", scanner.Text(), "died with", res.Status)
-		}
+		lines[lineCount] = make(chan string)
 
-		body, err := ioutil.ReadAll(res.Body)
-
-		if *printResponses {
-			fmt.Println(string(body))
-		}
-
-		hasher := md5.New()
-		hasher.Write(body)
-		hash := hex.EncodeToString(hasher.Sum(nil))
-
-		fmt.Println(scanner.Text() + ": " + hash)
-		outputMap[lineCount] = scanner.Text() + ": " + hash + "\n"
+		wg.Add(1)
+		go fetch(&wg, lineCount, url, *printResponses, lines[lineCount])
 
 		lineCount++
 	}
 
+	output := ""
+	for i := 0; i < lineCount; i++ {
+		output += <- lines[i] + "\n"
+	}
+
+	wg.Wait()
+
 	fmt.Println("Elapsed:", time.Since(start))
 
-	output := filepath.Join(currDir, *outputFile)
+	outputFp := filepath.Join(currDir, *outputFile)
 
-	err = ioutil.WriteFile(output, []byte(output), 0644)
+	err = ioutil.WriteFile(outputFp, []byte(output), 0644)
 	handleErr(err)
 
-	fmt.Println("Done, wrote to: " + output)
+	fmt.Println("Done, wrote to: " + outputFp)
+}
+
+func fetch(wg *sync.WaitGroup, count int, url string, printResponses bool, ch chan string) {
+	defer wg.Done()
+
+	res, err := http.Get(url)
+	handleErr(err)
+	defer res.Body.Close()
+
+	if res.StatusCode >= 300 {
+		log.Fatal("Request", url, "died with", res.Status)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if printResponses {
+		fmt.Println(string(body))
+	}
+
+	hasher := md5.New()
+	hasher.Write(body)
+	hash := hex.EncodeToString(hasher.Sum(nil))
+
+	response := url + ": " + hash
+	fmt.Println(response)
+
+	ch <- response
 }
 
 func handleErr(err error) {
