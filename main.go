c010c7c66de27768033313a6610bbe5f02155bbc
Dev Kordes <devkordes+github@gmail.com>
2020-07-21 00:36:23 +0200

    Adds option to compare to a file

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 func main() {
 	inputFile := flag.String("input", "", "Input file")
 	outputFile := flag.String("output", "", "Output file")
+	compareToFile := flag.String("compareTo", "", "Compare to file")
 	printResponses := flag.Bool("print", false, "Whether to print responses. Default: no")
 	concurrent := flag.Int("concurrent", 1, "Concurrent requests. Default: 1")
 
@@ -32,11 +33,18 @@ func main() {
 
 	currDir, err := os.Getwd()
 
-	inputFp := filepath.Join(currDir, *inputFile)
+	*inputFile = filepath.Join(currDir, *inputFile)
 
-	fmt.Println("Reading from", inputFp)
+	if *compareToFile != "" {
+		*compareToFile = filepath.Join(currDir, *compareToFile)
+		if _, err := os.Stat(*compareToFile); err != nil {
+			log.Fatal("CProblem with compare file", *compareToFile)
+		}
+	}
 
-	file, err := os.Open(inputFp)
+	fmt.Println("Reading from", *inputFile)
+
+	file, err := os.Open(*inputFile)
 	handleErr(err)
 	defer file.Close()
 
@@ -59,22 +67,44 @@ func main() {
 		lineCount++
 	}
 
+	var compareCh chan []string
+	var compareLines []string
+
+	if *compareToFile != "" {
+		compareCh = make(chan []string)
+
+		go readCompareFile(compareToFile, compareCh)
+	}
+
+	if compareCh != nil {
+		compareLines = <- compareCh
+	}
+
 	output := ""
 	for i := 0; i < lineCount; i++ {
-		output += <-lines[i] + "\n"
+		line := <- lines[i]
+		output += line + "\n"
+
+		if compareLines != nil {
+			if len(compareLines) <= i {
+				fmt.Println("Comparefile does not have line", i)
+			} else if compareLines[i] != line {
+				log.Fatal("Line ", i, " does not match\nOutput:\n", line, "\nComparison:\n", compareLines[i])
+			}
+		}
 	}
 
 	fmt.Println("Elapsed:", time.Since(start))
 
-	outputFp := filepath.Join(currDir, *outputFile)
+	*outputFile = filepath.Join(currDir, *outputFile)
 
-	err = ioutil.WriteFile(outputFp, []byte(output), 0644)
+	err = ioutil.WriteFile(*outputFile, []byte(output), 0644)
 	handleErr(err)
 
-	fmt.Println("Done, wrote to: " + outputFp)
+	fmt.Println("Done, wrote to: " + *outputFile)
 }
 
-func fetch(url string, printResponses bool, ch chan string, sem <-chan struct{}) {
+func fetch(url string, printResponses bool, ch chan<- string, sem <-chan struct{}) {
 	res, err := http.Get(url)
 	handleErr(err)
 	defer res.Body.Close()
@@ -104,4 +134,20 @@ func handleErr(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func readCompareFile(compareToFile *string, compareCh chan<- []string) {
+	file, err := os.Open(*compareToFile)
+	handleErr(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	handleErr(err)
+
+	compareLines := []string{}
+	for scanner.Scan() {
+		compareLines = append(compareLines, scanner.Text())
+	}
+
+	compareCh <- compareLines
 }
